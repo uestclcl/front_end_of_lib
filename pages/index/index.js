3bae113a92b3632d2bb11546207a5821abf4b73e
liuchanglei <uestclcl@qq.com>
2019-12-30 19:03:21 +0800

    111

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,3 +1,5 @@
+import i18n from '../../i18n/index'
+const iGetUserInfo = i18n.get_user_info
 const app = getApp()
 
 Page({
@@ -16,6 +18,34 @@ Page({
   onTapLendBook:function(e){
     var that=this;
     console.log("借书成功");
+    tt.login({
+			success: function (res) {
+				tt.getUserInfo({
+					withCredentials: that.data.withCredentials,
+					success: function (res) {
+						console.log('getUserInfo success')
+						console.log(arguments);
+						tt.showToast({
+							title: 'success'
+						});
+						that.setData({
+							hasUserInfo: true,
+							userInfo: res.userInfo,
+							rawData: res.rawData ? res.rawData : "",
+							signature: res.signature ? res.signature : "",
+							encryptedData: res.encryptedData ? res.encryptedData : "",
+							iv: res.iv ? res.iv : ""
+						});
+					},
+					fail() {
+						console.log('getUserInfo fail')
+					}
+				});
+			}, fail: function () {
+				console.log(`login fail`);
+			}
+		});
+    
     tt.request({
       url: 'http://127.0.0.1:8080/user/login', // 目标服务器url
       data:{
